ba7eb258e025dad2586eb4f6739fcca726178c8d
Sandeep Bhatt <50487296+Sandeepbhatt3287@users.noreply.github.com>
2021-08-13 10:28:16 +0530

    Fibonacci Number

---
+++ Getting started/Fibonacci Number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char** agrc){
+
+ //write your code here
+ int l,a=0,b=1;
+ cin>>l;
+
+    
+for(int n =0;n<l;n++)
+{ cout<<"\n"<<a;
+   int c=a+b;
+    a=b;
+    b=c;  
+    
+}
+}
