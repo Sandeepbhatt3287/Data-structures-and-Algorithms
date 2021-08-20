c7a7436598b39ae44ee74c5097c810555229b286
Sandeep Bhatt <50487296+Sandeepbhatt3287@users.noreply.github.com>
2021-08-20 22:36:19 +0530

    prime Factorisation

---
+++ Getting started/Prime Factorisation of a number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char** agrc){
+	
+
+ //write your code here
+int n;
+cin>>n;
+
+for (int div =2; div*div<=n;div++)
+    {
+    	while(n%div==0)
+    	{  
+		  n=n/div;
+    	  cout<<div;
+		}
+	}
+	if(n!=1)
+	{
+		cout<<n;
+	}
+}
+
