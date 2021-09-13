fff58f59078199f228170f7d0edc938b8767f653
Sandeep Bhatt <50487296+Sandeepbhatt3287@users.noreply.github.com>
2021-09-13 19:46:19 +0530

    Reverse of array

--- Function and Arrays/Reverse an array.cpp
+++ Function and Arrays/Reverse an array.cpp
@@ -1,22 +1,46 @@
-//1. You are given a number n, representing the size of array a.
-//2. You are given n numbers, representing elements of array a.
-//3. You are required to reverse the contents of array a.
-
-
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int main(int argc, char** agrc){
 
- //write your code here
-    int n1;
-     cin>>n1;
-    int a[n1];
-    
-    for (int i=0; i<n1;i++)
+
+void reverse (int* a,int i, int j)
+{
+    while (i<j)
     {
+         int temp = a[i];
+         a[i]=a[j];
+         a[j]=temp;
+         ++i;
+         --j;
+    }
+}
+void rotate(int* a, int n, int k){
+   // write your code here
+    // int size= sizeof(a)/sizeof(a[0]);
+   k= k%n;
+    // cout<<"k"<<size<<endl;
+  if(k<0)
+  k+=n;
+  
+  reverse (a,0,n-k-1);
+  reverse (a,n-k,n-1);
+  reverse(a,0,n-1);
+}
+
+void display(int* a, int n){
+    for(int i = 0 ; i < n; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n, r;
+    cin>>n;
+    int* a = new int[n];
+    for(int i = 0 ; i < n; i++){
         cin>>a[i];
     }
- for (int j=n1-1;j>-1;j--)
-     // cout<<j;
-     cout<<a[j];
-} 
+    cin>>r;
+    rotate(a,n,r);
+    display(a,n);
+}
