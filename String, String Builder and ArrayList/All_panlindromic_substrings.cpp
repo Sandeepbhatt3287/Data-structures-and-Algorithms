5556cb79de9646240a4232cb2549e628796ba390
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-09-24 17:23:39 +0530

    panlindromic substrings

--- String, String Builder and ArrayList/All_panlindromic_substrings.cpp
+++ String, String Builder and ArrayList/All_panlindromic_substrings.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 
 
+
+bool isPal( string str)
+ {    
+     int li=0,ri=str.length()-1;
+     while (li<ri)
+   {
+    if (str[li]!=str[ri])
+         return false;
+         
+     li++;
+     ri--;
+    }
+  return true;
+}
+
+void solve(string str)
+{  
+    string temp;
+for (int i =0;i<str.length();i++)
+   { for (int j =1;j<=str.length()-i;j++)
+     {
+        temp = str.substr(i,j);
+           // string str1 = str[i];
+
+
+       if (isPal(temp)==true)
+          {
+              cout<<temp<<endl;
+          }
+
+     }
+   }
+}
 int main(int argc, char** agrc)
 {
 
     string str;
     getline(cin,str);
-
-
-     int li=0,ri=str.length();
-
-
-bool isPal( string &ss)
- { 
-     while (li<ri)
-   {
-    if (ss[li]!=ss[ri])
-     return false;
-     li++;
-     ri--;
-    }
-  return true;
-};
-
-
-
-for (int i =0;i<ri;i++)
-   { for (int j =i+1;j<ri;j++)
-     {
-        string ss = str.substr(i,j);
-           // string str1 = str[i];
-
-
-       if (isPal(ss)==true)
-          {
-              cout<<ss<<endl;
-          }
-
-     }
-   }
-   
+    solve(str);
 }
