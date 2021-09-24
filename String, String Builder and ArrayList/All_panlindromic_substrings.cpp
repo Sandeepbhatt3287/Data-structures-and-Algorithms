00c5b8d1bad35788dc81090fa40076dce32ee36d
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-09-24 16:33:37 +0530

    facing some errors

--- String, String Builder and ArrayList/All_panlindromic_substrings.cpp
+++ String, String Builder and ArrayList/All_panlindromic_substrings.cpp
@@ -1,7 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 int main(int argc, char** agrc){
 
-string s1="sandeep";
-cout<<s1;
+string str;
+getline(cin,str);
+
+
+int li=0,ri=str.length();
+
+int Palindrom(string &ss)
+{  while (li<ri)
+   {
+    if (str[li]!=str[ri])
+     return 0;
+
+     li++;
+     ri--;
+  }
+  return 1;
 }
+
+
+for (int i =0;i<ri;i++)
+   { for (int j =i+1;j<ri;j++)
+     {
+        string ss = str.substr(i,j);
+           // string str1 = str[i];
+       if (Palindrom(ss)==1)
+          {
+              cout<<ss<<endl;
+          }
+
+     }
+   }
+   
+ }
