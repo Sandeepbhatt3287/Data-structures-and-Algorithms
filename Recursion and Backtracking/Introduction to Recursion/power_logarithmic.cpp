5b2f239ae108f8ab3bd9152d22fa5981ee4fa261
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-09-28 22:12:42 +0530

    power logarithmic

--- Recursion and Backtracking/Introduction to Recursion/power_logarithmic.cpp
+++ Recursion and Backtracking/Introduction to Recursion/power_logarithmic.cpp
@@ -5,7 +5,10 @@ int power(int x,int n){
    // write your code here
    if (n==0)
    return 1;
-   return x*power(x,n-1);
+   if (n%2==1)
+   return x*power(x,n/2)*power(x,n/2);
+   else
+   return power(x,n/2)*power(x,n/2);
    
 }
 
