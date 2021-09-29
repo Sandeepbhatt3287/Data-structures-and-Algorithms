a84a09c5fc08c1a5b20decf2889b3e59173b8cb9
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-09-29 22:10:13 +0530

    Display array reverse using recursion

---
+++ Recursion and Backtracking/Recursion_in_arrays/Display_array_reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int idx, int n){
+    // write your code here
+    if (idx==n)
+    return;
+    
+    cout<<arr[n-1]<<endl;  //starting fro the end
+    display(arr,idx,n-1);
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    display(arr, 0, n);
+}
