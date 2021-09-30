6b25b4e169fea9a94dbbfd62cdb7f4671635120d
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-01 01:27:41 +0530

    basic structure

---
+++ Recursion and Backtracking/Recursion with arraylist/Get_subsequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> gss(string s){
+    // write your code here
+    
+}
+
+int main(){
+    string s;
+    cin >> s;
+    vector<string> ans = gss(s);
+    int cnt = 0;
+
+    cout << '[';
+    for (string str : ans){
+        if (cnt != ans.size() - 1)
+            cout << str << ", ";
+        else
+            cout << str;
+        cnt++;
+    }
+    cout << ']';
+}
