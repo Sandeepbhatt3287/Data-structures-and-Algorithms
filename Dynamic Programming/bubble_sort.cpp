21a5a4c54de7ec119dcbc43a255706716c31f183
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-03 19:17:32 +0530

    bubble sort

---
+++ Dynamic Programming/bubble_sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int max(int arr[], int idx, int n){
+    // write your code here
+  
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    bubble(arr[],n)
+}
