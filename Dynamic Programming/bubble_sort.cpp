0c96e19d77c715d30b234988d30afc283e520b0f
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-04 16:52:52 +0530

    Bubble sort with log details

--- Dynamic Programming/bubble_sort.cpp
+++ Dynamic Programming/bubble_sort.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int max(int arr[], int idx, int n){
+void bubble(int arr[], int n){
     // write your code here
-  
+      for (int i =1; i<n;i++)
+       for (int j =0;j<n-i;j++)
+       {  cout<<"Comparing "<<arr[j+1]<<" and "<<arr[j]<<endl;
+          if (arr[j+1]<arr[j])
+          {   cout<<"Swapping "<<arr[j+1]<<" and "<<arr[j]<<endl;
+               int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+         }
+     }
 
-}
+// }
+// void swap(int arr[],int i ,int j)
+// {
+//     int temp=arr[i];
+//     arr[i]=arr[j];
+//     arr[j]=temp;
+// }
 
 int main(){
     int n;
@@ -13,5 +29,10 @@ int main(){
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    bubble(arr[],n)
+    
+    bubble(arr,n);
+    
+    
+     for (int i = 0; i < n; i++)
+        cout<<arr[i]<<endl;
 }
