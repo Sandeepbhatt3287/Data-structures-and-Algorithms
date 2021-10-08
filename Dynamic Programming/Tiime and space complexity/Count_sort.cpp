80990d9542a0aa0d382ba86a5c75872fdecae612
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-08 14:14:20 +0530

    updating main function

--- Dynamic Programming/Tiime and space complexity/Count_sort.cpp
+++ Dynamic Programming/Tiime and space complexity/Count_sort.cpp
@@ -39,5 +39,15 @@ using namespace std;
 
 int main()
 {
+    int n;
+
+    cin>>n;
+    int a[n];
+    for (int  i = 0; i < n; i++)
+    cin>>>a[i];
+    int max,min;
+
+    countsort(a,min,max,n);
+
 
 }
