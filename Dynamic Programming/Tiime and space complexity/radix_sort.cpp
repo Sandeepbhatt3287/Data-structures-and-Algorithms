e5a422d178bc63539d409b594ed6491ee95d4214
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-08 22:18:07 +0530

    updating countsort function

--- Dynamic Programming/Tiime and space complexity/radix_sort.cpp
+++ Dynamic Programming/Tiime and space complexity/radix_sort.cpp
@@ -10,24 +10,30 @@ using namespace std;
 
 
 
-void countSort(vector<int>& arr)
+void countSort(vector<int>& arr,int exp)
 {
 	int max = *max_element(arr.begin(), arr.end());
-	int min = *min_element(arr.begin(), arr.end());
+	// int min = *min_element(arr.begin(), arr.end());
 	// int range = max - min + 1;
 
 	vector<int> count(10), output(arr.size());
-	for (int i = 0; i < arr.size(); i++)
-		count[arr[i]]++;
 
+//   makeing frequency array
+	for (int i = 0; i < arr.size(); i++)
+		count[arr[i]/exp%10]++;
+
+//  convert it into prefix sum array
 	for (int i = 1; i < count.size(); i++)
 		count[i] += count[i - 1];
 
+// stable sorting 
 	for (int i = arr.size() - 1; i >= 0; i--) {
-		output[count[arr[i]] - 1] = arr[i];
-		count[arr[i]]--;
+		output[count[arr[i]/exp%10] - 1] = arr[i];
+		count[arr[i]/exp%10]--;
 	}
 
+
+//  fiilling original array with helpof output array
 	for (int i = 0; i < arr.size(); i++)
 		arr[i] = output[i];
 }
@@ -44,6 +50,7 @@ void printArray(vector<int>& arr)
 void radixsort(vector<int>& a)
 {
   int max = *max_element(arr.begin(), arr.end());
+
      
 }
 
