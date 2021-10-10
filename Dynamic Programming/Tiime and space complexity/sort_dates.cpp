47dbd74d61bbfdf1af9d58ada96f5d93df3fdccf
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-10 18:38:10 +0530

    using redix sort logic

--- Dynamic Programming/Tiime and space complexity/sort_dates.cpp
+++ Dynamic Programming/Tiime and space complexity/sort_dates.cpp
@@ -1,60 +1,83 @@
+// Counting sort which takes negative numbers as well
+#include <algorithm>
 #include <iostream>
-
 #include <vector>
-
 using namespace std;
 
-void input(vector<int> &arr)
+
+
+
+
+
+
+void countSort(vector<int>& arr,int exp)
 {
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cin >> arr[i];
-    }
+	int max = *max_element(arr.begin(), arr.end());
+	// int min = *min_element(arr.begin(), arr.end());
+	// int range = max - min + 1;
+  cout<<"After sorting on "<<exp<<" place -> ";
+  
+	vector<int> count(10), output(arr.size());
+
+//   makeing frequency array
+	for (int i = 0; i < arr.size(); i++)
+		count[arr[i]/exp%10]++;
+
+//  convert it into prefix sum array
+	for (int i = 1; i < count.size(); i++)
+		count[i] += count[i - 1];
+
+// stable sorting 
+	for (int i = arr.size() - 1; i >= 0; i--) {
+		output[count[arr[i]/exp%10] - 1] = arr[i];
+		count[arr[i]/exp%10]--;
+	}
+
+
+//  fiilling original array with helpof output array
+	for (int i = 0; i < arr.size(); i++)
+		arr[i] = output[i];
 }
 
-void print(vector<int> &arr)
+void printArray(vector<int>& arr)
 {
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << endl;
-    }
-    cout << endl;
+    
+	for (int i = 0; i < arr.size(); i++)
+		cout << arr[i]<<" ";
+	cout << "\n";
 }
 
-// used for swapping ith and jth elements of array
-void swap(vector<int> &arr, int i, int j)
-{
-    cout << ("Swapping index " + to_string(i) + " and index " + to_string(j)) << endl;
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
 
-void sort01(vector<int> &arr)
-{
-    int i=0,j=0;
-    while (i<arr.size())
-    {
-        if(arr[i]==0)
-        {
-          swap(arr,i,j);
-          i++;
-          j++;
-        }else{
-            i++;
-        }
-    }
 
+void radixsort(vector<int>& a)
+{
+  int max = *max_element(a.begin(), a.end());
+
+  int exp =1;
+  while (exp<=max)
+  {
+      countSort(a,exp);
+      exp=exp*10;
+
+  }
+
+     
 }
 
 int main()
-{
-    int n, m;
-    cin >> n;
-    vector<int> A(n, 0);
-    input(A);
-
-    sort01(A);
-    print(A);
-    return 0;
+{   int n;
+    cin>>n;
+	vector<int> arr;
+	
+	for (int i = 0; i < n; i++) 
+        {
+            int temp;
+            cin>>temp;
+            arr.push_back(temp);
+        }
+	
+	radixsort(arr);
+	printArray(arr);
+	printArray(arr);
+	return 0;
 }
