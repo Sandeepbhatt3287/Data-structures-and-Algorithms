c70bc3de7a0d48954313f20b32fab5c6b6847d90
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-12-09 17:12:33 +0530

    first draft

---
+++ String, String Builder and ArrayList/string_with_difference_of_every_two_consecutive_char.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+void printstr(string str)
+{
+  //write your code here
+  
+
+}
+int main()
+{
+  string str;
+  getline(cin, str);
+  cout<<printstr(str);
+  return 0;
+}
