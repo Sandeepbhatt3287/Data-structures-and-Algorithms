8c9f12d2ee7ae749694d8992977a1ba446f31c56
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-12-11 18:09:55 +0530

    recursion final code

--- String, String Builder and ArrayList/permutation_of_string.cpp
+++ String, String Builder and ArrayList/permutation_of_string.cpp
@@ -1,16 +1,36 @@
-#include<iostream>
-#include<string>
+
+#include <bits/stdc++.h>
 using namespace std;
 
-
-void printallperm(string str)
+void permute(string a, int l, int r)
 {
-  //write your code here
+	// Base case
+	if (l == r)
+		cout<<a<<endl;
+	else
+	{
+		// Permutations made
+		for (int i = l; i <= r; i++)
+		{
+
+			// Swapping done
+			swap(a[l], a[i]);
+
+			// Recursion called
+			permute(a, l+1, r);
+
+			//backtrack
+			swap(a[l], a[i]);
+		}
+	}
 }
+
+// Driver Code
 int main()
 {
-  string str;
-  getline(cin, str);
-
-  printallperm(str);
+	string str;
+    getline(cin,str);
+	int n = str.size();
+	permute(str, 0, n-1);
+	return 0;
 }
