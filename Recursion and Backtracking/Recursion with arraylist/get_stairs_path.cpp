018382c3418a6b1c509dca1b8afa25a31b6724a9
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-12-17 17:38:21 +0530

    final code

--- Recursion and Backtracking/Recursion with arraylist/get_stairs_path.cpp
+++ Recursion and Backtracking/Recursion with arraylist/get_stairs_path.cpp
@@ -3,23 +3,47 @@
 #include<string>
 using namespace std;
 
-vector<string> get_stair_paths(int n) {
-  //Write your code here
+// vector<string> get_stair_paths(int n) {
+//   //Write your code here
+// }
+
+// int main() {
+//   int n;
+//   cin >> n;
+//   vector<string> ans = get_stair_paths(n);
+//   int cnt = 0;
+
+//   cout << '[';
+//   for (string str : ans) {
+//     if (cnt != ans.size() - 1)
+//       cout << str << ", ";
+//     else
+//       cout << str;
+//     cnt++;
+//   }
+//   cout << ']';
+// }
+
+
+void printStairPaths(int n, string psf){
+    // write your code here
+
+	if(n<=0)
+	{
+		cout<<psf<<"\n";
+       return;
+	}
+    
+    for(int jump=1;jump<=3 and n-jump>=0;jump++)
+    {
+    	printStairPaths(n-jump,psf + to_string(jump));
+    }
+    
 }
 
-int main() {
-  int n;
-  cin >> n;
-  vector<string> ans = get_stair_paths(n);
-  int cnt = 0;
+int main(){
 
-  cout << '[';
-  for (string str : ans) {
-    if (cnt != ans.size() - 1)
-      cout << str << ", ";
-    else
-      cout << str;
-    cnt++;
-  }
-  cout << ']';
+    int n;
+    cin >> n;
+    printStairPaths(n, "");
 }
