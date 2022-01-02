09aedfe5c3b6a6f99281713787cfa7de5878f092
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-02 16:27:32 +0530

    draft version

---
+++ Recursion and Backtracking/Recursion_way_up/print_kpc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+string codes[] = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+void printKPC(string ques, string asf){
+    // write your code here
+    
+}
+
+int main(){
+    string str;
+    cin >> str;
+    printKPC(str, "");
+}
