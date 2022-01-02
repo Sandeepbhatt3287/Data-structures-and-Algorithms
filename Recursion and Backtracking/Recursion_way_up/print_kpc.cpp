de5d6f167d517d968dbb943e137ae78e6dbfda21
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-02 17:10:55 +0530

    final code

--- Recursion and Backtracking/Recursion_way_up/print_kpc.cpp
+++ Recursion and Backtracking/Recursion_way_up/print_kpc.cpp
@@ -3,9 +3,25 @@ using namespace std;
 
 string codes[] = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
 
-void printKPC(string ques, string asf){
+void printKPC(string ques, string ans){
     // write your code here
+
+     if(ques.size()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
     
+    char ch_option =ques.at(0);
+     string roq = ques.substr(1);
+
+     string codeforch =codes[ch_option - '0'];
+
+     for(int i =0; i< codeforch.size(); i++)
+     {
+         char cho = codeforch.at(i);
+         printKPC(roq,ans + cho);
+     }
 }
 
 int main(){
