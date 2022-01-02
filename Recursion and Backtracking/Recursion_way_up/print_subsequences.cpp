90af5d7ffb3efafef999ae2a3aceb099f965667a
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-02 14:02:15 +0530

    draft version

---
+++ Recursion and Backtracking/Recursion_way_up/print_subsequences.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printSS(string ques, string ans){
+    // write your code here
+    
+}
+
+int main(){
+    string str;
+    cin >> str;
+    printSS(str, "");
+}
