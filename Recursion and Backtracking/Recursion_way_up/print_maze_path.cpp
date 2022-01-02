c38191edc13a868df293158304a2400a231641b3
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-02 22:13:47 +0530

    draft version

---
+++ Recursion and Backtracking/Recursion_way_up/print_maze_path.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printMazePaths(int sr, int sc, int dr, int dc, string psf){
+    // write your code here
+    
+}
+
+int main(){
+    int n;
+    int m;
+    cin >> n >> m;
+    printMazePaths(0, 0, n - 1, m - 1, "");
+}
