0a2963c58d31909f3bcbe20b2591c3a7514dfd55
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-02 22:07:01 +0530

    final code

--- Recursion and Backtracking/Recursion_way_up/print_stair_path.cpp
+++ Recursion and Backtracking/Recursion_way_up/print_stair_path.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void printStairPaths(int n, string psf){
+void printStairPaths(int n, string path){
     // write your code here
+    
+    // base case
+    if (n<0)
+        return;
+    if(n==0)
+    {
+        cout<<path<<endl;
+        return;
+    }
+    
+    printStairPaths(n-1,path + "1");
+    printStairPaths(n-2,path + "2");
+    printStairPaths(n-3,path + "3");
+    
     
 }
 
