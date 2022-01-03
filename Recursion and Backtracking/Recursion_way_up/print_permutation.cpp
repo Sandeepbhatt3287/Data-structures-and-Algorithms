2cf8c5cf10170db024749ae47e52538c332a5c51
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-03 17:12:11 +0530

    final code

--- Recursion and Backtracking/Recursion_way_up/print_permutation.cpp
+++ Recursion and Backtracking/Recursion_way_up/print_permutation.cpp
@@ -4,6 +4,23 @@ using namespace std;
 void printPermutations(string str, string asf){
     // write your code here
 
+    if(str.size()== 0)
+    {   
+        cout<<asf<<endl;
+        return;
+    }
+    
+    for(int i=0;i<str.size();i++)
+    {
+        char ch =str.at(i);
+        string leftstr = str.substr(0,i);
+        string rigthstr = str.substr(i+1);
+        string roq = rigthstr + leftstr;
+        
+        printPermutations(roq,asf+ch);
+        
+    }
+    
 }
 
 int main(){
