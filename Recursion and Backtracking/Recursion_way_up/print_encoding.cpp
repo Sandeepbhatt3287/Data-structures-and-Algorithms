1a154a9daa3eda6ce8eed0764f0f82cf8ee7ab68
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-03 19:13:00 +0530

    final code

--- Recursion and Backtracking/Recursion_way_up/print_encoding.cpp
+++ Recursion and Backtracking/Recursion_way_up/print_encoding.cpp
@@ -2,8 +2,53 @@
 #include<string>
 using namespace std;
 
-void printEncoding(string str, string asf){
+void printEncoding(string ques, string asf){
     // write your code here
+    
+    if(ques.size() == 0)
+    {
+     cout<<asf<<endl;
+      return;
+    }  else if(ques.size()==1)
+    { 
+        char ch =ques.at(0);
+        if(ch =='0')
+        {
+            return;
+        }
+            else{
+                int chv = ch - '0';
+                char code = char ('a' + chv -1);
+                // asf = asf + code ;
+                cout<<(asf+code)<<endl;
+            }
+    }  else 
+    {
+        char ch = ques.at(0);
+        
+        string roq = ques.substr(1);
+        
+        if(ch == '0')
+            return ;
+        else
+        {
+            int chv =ch-'0';
+            char code = (char)('a'+chv - 1);
+            printEncoding(roq, asf + code);
+        }
+        
+        string ch12 = ques.substr(0,2);
+        string roq12 = ques.substr(2);
+        
+        int ch12v;
+        ch12v = stoi(ch12);       //using type converting string to integer
+        
+        if(ch12v<=26)
+        {
+            char code = (char)('a'+ ch12v -1);
+            printEncoding(roq12,asf+code);
+        }
+    }
     
 }
 
