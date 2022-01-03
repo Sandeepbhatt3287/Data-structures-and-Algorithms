f72b0e00748516e53e9ae5b7ba18b714e7b22812
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-03 17:28:55 +0530

    draft version

---
+++ Recursion and Backtracking/Recursion_way_up/print_encoding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+void printEncoding(string str, string asf){
+    // write your code here
+    
+}
+
+int main(){
+    string str;
+    cin>>str;
+    printEncoding(str,"");
+    
+}
