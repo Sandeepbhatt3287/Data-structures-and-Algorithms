480caf51d601e0e017b6bc6e8fdd4fa74a779921
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-04 17:44:32 +0530

    draft version

---
+++ Basic Data structures/stacks and queues/stock_span.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void display(vector<int>a) {
+  for (int i = 0; i < a.size(); i++)
+  {
+    cout << a[i] << endl;
+  }
+}
+vector<int> solve(vector<int>arr)
+{
+  //write your code here
+}
+int main(int argc, char** argv)
+{
+  int n;
+  cin >> n;
+  vector<int>arr(n, 0);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  vector<int>span(n, 0);
+  span = solve(arr);
+  display(span);
+  return 0;
+}
