92c6ed54c01ff2ebd97b7a9b97fe8dd94c625d3b
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-15 21:39:35 +0530

    final code

--- Basic Data structures/stacks and queues/infix_evaluation.cpp
+++ Basic Data structures/stacks and queues/infix_evaluation.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 #include <ctype.h>
+
 using namespace std;
+
+
 
 
 int operation(int v1,int v2,char optor)
@@ -17,6 +20,9 @@ int operation(int v1,int v2,char optor)
   }
 
 
+
+
+
 int precedence(char optor)
   {
     if(optor=='+')
@@ -30,73 +36,116 @@ int precedence(char optor)
   }  
 
 
+
+
+
+
 int main() {
   string str;
   getline(cin, str);
-  // write your code here
-   
-    stack<int> opd;
-    stack<char> opt;
-    
-    for (int i=0;i<str.size();i++)
-    {
-        char ch = str[i];
-        
-        if(ch=='(')
-        {
-         opt.push(ch);   
-        }
-        else if(isdigit(ch))
-        {
-            opd.push(ch-'0');     // char into int
-        }
-        else if(ch==')')
-        {
-            while (opt.top()!= '(')
-            {
-                char optor =opt.top();
-                opt.pop();
-                int v2 = opd.top();
-                opd.pop();
-                int v1 = opd.top();
-                opd.pop();
-                int  opv = operation(v1,v2,optor);
-                
-                opd.push(opv);
-            }
-            opt.pop();
-        }
-        else if (ch=='+' || ch=='-' || ch=='*'|| ch=='/')
-        {
-      while (opt.size()>0 && opt.top()!='(' && precedence(ch)<= precedence(opt.top()))
-                   {
-                char optor =opt.top();
-                opt.pop();
-                int v2 = opd.top();
-                opd.pop();
-                int v1 = opd.top();
-                opd.pop();
-                int opv = operation(v1,v2,optor);
-                
-                opd.push(opv);
 
-                   }
-            opt.push(ch);
-        }
-        
-       }
-     while (opt.size()!= 0)
-            {
-                char optor = opt.top();
-                opt.pop();
-                int v2 = opd.top();
-                opd.pop();
-                int v1 = opd.top();
-                opd.pop();
-                int  opv = operation(v1,v2,optor);
-                opd.push(opv);
-            }
-   cout<<(opd.top());
+  // write your code here
+  
+  stack<string> post;
+  stack<string> pre;
+  stack<char> ops;
+  
+  for (int i =0;i<str.size();i++)
+  {
+      char ch = str[i];
+      
+      if(ch =='(')
+      {
+       ops.push(ch);   
+      }else if ((ch>='0' and ch <='9') || (ch>='a' and ch<='z')|| (ch >='A' and ch<='Z') )
+      {  
+          string res;
+          res+=ch;
+       post.push(res);
+       pre.push(res);
+      }else if (ch ==')')
+      {
+          while(ops.top()!='(')
+          {
+              char op = ops.top();
+              ops.pop();
+              string postv2= post.top();
+              post.pop();
+              string postv1= post.top();
+              post.pop();
+              
+              string postv = postv1 + postv2 + op;
+              
+              post.push(postv);
+              
+              string prev2= pre.top();
+              pre.pop();
+              string prev1= pre.top();
+              pre.pop();
+              
+              string prev = op + prev1 + prev2 ;
+              
+              pre.push(prev);
+              
+          }
+          ops.pop();
+          
+      }
+      else if(ch=='+'|| ch=='-'||ch=='*'||ch=='/')
+      {
+          while( ops.size()>0 and ops.top()!='(' and precedence(ch)<=precedence(ops.top()))
+          {
+              char op = ops.top();
+              ops.pop();
+              string postv2= post.top();
+              post.pop();
+              string postv1= post.top();
+              post.pop();
+              
+              string postv = postv1 + postv2 + op;
+              
+              post.push(postv);
+              
+              string prev2= pre.top();
+              pre.pop();
+              string prev1= pre.top();
+              pre.pop();
+              
+              string prev = op + prev1 + prev2;
+              
+              pre.push(prev);
+              
+          }
+          ops.push(ch);
+         
+      }
+  }
+   while(ops.size()>0)
+          {
+              char op = ops.top();
+              ops.pop();
+              string postv2= post.top();
+              post.pop();
+              string postv1= post.top();
+              post.pop();
+              
+              string postv = postv1 + postv2 + op;
+              
+              post.push(postv);
+              
+              string prev2= pre.top();
+              pre.pop();
+              string prev1= pre.top();
+              pre.pop();
+              
+              string prev = op+prev1 + prev2 ;
+              
+              pre.push(prev);
+              
+          }
+    
+         cout<<post.top()<<endl;
+         cout<<pre.top();
 
   return 0;
 }
