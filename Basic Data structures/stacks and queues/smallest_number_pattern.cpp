04a6847dc25ef0a54167cc1981f7e9838b74c8d2
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-18 20:23:19 +0530

    final code

--- Basic Data structures/stacks and queues/smallest_number_pattern.cpp
+++ Basic Data structures/stacks and queues/smallest_number_pattern.cpp
@@ -10,7 +10,33 @@ int main() {
   cin >> str;
 
   // write your code here
+  stack<int> st;
+  int num=1;
+  
+  for(int i=0;i<str.size();i++)
+  {
+      char ch = str[i];
+      if (ch =='d')
+      {
+          st.push(num);
+          num++;
+      }else
+      {
+          st.push(num);
+          num++;
+          while (st.size()>0)
+          {
+              cout<<st.top();
+              st.pop();
+          }
+      }
+  }
+   st.push(num);
+          while (st.size()>0)
+          {
+              cout<<st.top();
+              st.pop();
+          }
 
-
-
+  return 0;
 }
