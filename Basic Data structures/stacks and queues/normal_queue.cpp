1ab251b3dd866054d1cd819999c0349fc9e04e46
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-20 02:58:55 +0530

    draft version

---
+++ Basic Data structures/stacks and queues/normal_queue.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class CustomQueue {
+public:
+  int* arr;
+  int sizeQ;
+  int front;
+  int arrSize;
+
+  CustomQueue(int cap) {
+    sizeQ = 0;
+    front = 0;
+    arr = new int[cap];
+    arrSize = cap;
+  }
+
+  int size() {
+    // write your code here
+  }
+
+  void add(int data) {
+    // write your code here
+  }
+
+  int peek() {
+    // write your code here
+  }
+
+  int remove() {
+    // write your code here
+  }
+
+  void display() {
+    // write your code here
+  }
+};
+
+int main() {
+  int n;
+  cin >> n;
+  CustomQueue q(n);
+  string str;
+  cin >> str;
+
+  while (str != "quit") {
+    if (str == "add") {
+      int val;
+      cin >> val;
+      q.add(val);
+    }
+    else if (str == "remove") {
+      int val = q.remove();
+      if (val != -1) {
+        cout << val << endl;
+      }
+    }
+    else if (str == "peek") {
+      int val = q.peek();
+      if (val != -1) {
+        cout << val << endl;
+      }
+    }
+    else if (str == "size") {
+      cout << q.size() << endl;
+    }
+    else if (str == "display") {
+      q.display();
+    }
+    cin >> str;
+  }
+  return 0;
+}
+
