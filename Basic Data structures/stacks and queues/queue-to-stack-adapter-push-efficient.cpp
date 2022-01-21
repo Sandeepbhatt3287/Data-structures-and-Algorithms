bc1f573051d270e3df21bf5c1109253a0c8c1d73
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-21 19:02:16 +0530

    draft v1.0

--- Basic Data structures/stacks and queues/queue-to-stack-adapter-push-efficient.cpp
+++ Basic Data structures/stacks and queues/queue-to-stack-adapter-push-efficient.cpp
@@ -10,18 +10,56 @@ public:
 
   int size() {
     // write your code here
+    return mainQ.size();
   }
 
   void push(int data) {
     // write your code here
+    mainQ.add(data);
   }
 
   int top() {
     // write your code here
+    if(size()==0)
+    {
+        cout<<"Stack overflow"<<endl;
+        return -1;
+    }else
+    {
+         while(mainQ.size()>1)
+        {
+            helperQ.add(mainQ.remove());
+        }
+        int val = mainQ.remove();
+        helperQ.add(val);
+        
+        while (helperQ.size()>0)
+        {
+            mainQ.add(helperQ.remove());
+        }
+        return val;
+    }
   }
 
   int pop() {
     // write your code here
+    if(size()==0)
+    {
+        cout<<"Stack overflow"<<endl;
+        return -1;
+    }else 
+    {
+        while(mainQ.size()>1)
+        {
+            helperQ.add(mainQ.remove());
+        }
+        int val = mainQ.remove();
+        while (helperQ.size()>0)
+        {
+            mainQ.add(helperQ.remove());
+        }
+        return val;
+    }
   }
 };
 
