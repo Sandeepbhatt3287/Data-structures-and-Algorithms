5ef0460b69b389c25990a18eaca43feb08948d4c
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-24 00:57:01 +0530

    Final code

--- Basic Data structures/stacks and queues/Linked List/remove-first-linked-list.cpp
+++ Basic Data structures/stacks and queues/Linked List/remove-first-linked-list.cpp
@@ -51,6 +51,18 @@ public:
   }
   void removeFirst() {
     //write your code here
+    if(size==0)
+    {
+        cout<<"List is empty"<<endl;
+    }else if(size==1)
+    {
+        head =tail=NULL;
+        size=0;
+    }else{
+        head =head->next;
+        size--;
+    }
+    
   }
 
 
