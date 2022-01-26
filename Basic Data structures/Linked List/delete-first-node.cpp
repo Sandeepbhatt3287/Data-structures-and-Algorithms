25721ab3c802928af88851ad3f1d167ea9ac39b8
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-27 02:19:48 +0530

    final code

---
+++ Basic Data structures/Linked List/delete-first-node.cpp
@@ -0,0 +1,83 @@
+// Insert at the beginning of singly linked list
+
+
+
+#include <iostream>
+using namespace std;
+struct node {
+// public:
+  int val;
+  node* next;
+    
+    node(int x)
+    {
+        val=x;
+        next=NULL;
+    }
+    
+};
+
+node *insertend( node *head,int x)
+{
+    
+    node *temp =new node(x);
+    
+    if(head==NULL)
+        return temp;
+    
+    node *cur=head;
+    while(cur->next!=NULL)
+        cur=cur->next;
+    
+    cur->next=temp;
+    
+     return head;
+        
+}
+
+node *deletehead(node *head,int x)
+{
+    if(head ==NULL)
+        return NULL;
+    else{
+        node *temp=head->next;
+        
+        delete head;
+        
+        return temp;
+    }
+}
+
+void print( node *head)
+{
+  if(head==NULL)
+      return;
+    
+    cout<<head->val<<endl;
+
+    // recursive call 
+print(head->next);
+        
+}
+
+
+int main(){
+    
+    node *head= NULL;
+    
+    
+    head= insertend(head,10);
+    head= insertend(head,20);
+    head= insertend(head,30);
+    head= insertend(head,999999);
+    
+     print(head);
+  
+    cout<<"\nLinked list after delete "<<endl;
+    
+    head=deletehead(head,10);
+    
+    print(head);
+    return 0;
+    
+}
