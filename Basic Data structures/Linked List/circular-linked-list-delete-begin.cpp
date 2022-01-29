8a3dca32ea91872ec7ee0b48eb7761ee98dc9037
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-01-30 02:36:53 +0530

    final code

---
+++ Basic Data structures/Linked List/circular-linked-list-delete-begin.cpp
@@ -0,0 +1,79 @@
+// using GFG 
+#include <iostream>
+using namespace std;
+
+
+struct node {
+  int data;
+  node*next;
+  // node*prev;
+    
+ node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+node *Delete_begin(node *head)
+  { 
+    
+       if(head==NULL)
+       {  
+           
+           return NULL;
+       }
+    if(head->next==NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    
+    node *cur=head;
+    
+    while(cur->next!=head)
+        cur=cur->next;
+    
+    cur->next=head->next;
+    delete head;
+    return cur->next;
+  }
+    
+  
+void print( node *head)
+{
+  if(head==NULL)
+      return;
+ cout<<head->data<<endl;
+
+    
+//     method 2
+      node *p=head->next;
+    do{
+        cout<<p->data<<endl;
+        p=p->next;
+        
+    }while(p!=head);
+}
+
+
+int main(){
+    
+    node *head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    head->next->next->next=head;
+
+    
+    cout<<"\nOriginal circular LL"<<endl;
+    print(head);
+    
+    head=Delete_begin(head);
+    cout<<"\nCircular  LL after Delete in begin "<<endl;
+    
+    print(head);
+    return 0;
+    
+}
