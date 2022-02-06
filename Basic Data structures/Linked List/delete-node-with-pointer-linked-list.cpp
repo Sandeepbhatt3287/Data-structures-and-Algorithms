dfc2f90c8bc528772bac5f02d6c0455eb9172de1
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-02-06 20:26:21 +0530

    final code

--- Basic Data structures/Linked List/delete-node-with-pointer-linked-list.cpp
+++ Basic Data structures/Linked List/delete-node-with-pointer-linked-list.cpp
@@ -1,3 +1,86 @@
 // delete node with only pointer given to it
 
 
+// Using floyd cycle detection
+
+
+#include <iostream>
+using namespace std;
+struct node {
+// public:
+  int val;
+  node* next;
+    
+    node(int x)
+    {
+        val=x;
+        next=NULL;
+    }
+    
+};
+
+
+node *insertend( node *head,int x)
+{
+    
+    node *temp =new node(x);
+    
+    if(head==NULL)
+        return temp;
+    
+    node *cur=head;
+    while(cur->next!=NULL)
+        cur=cur->next;
+    
+    cur->next=temp;
+    
+     return head;
+        
+}
+
+void deletenode(node *ptr)
+  {
+    node *temp=ptr->next;
+    ptr->val=temp->val;
+    ptr->next=temp->next;
+    delete(temp);
+  }
+
+
+
+
+void print( node *head)
+{
+  if(head==NULL)
+      return;
+    
+    cout<<head->val<<endl;
+
+    // recursive call 
+print(head->next);
+        
+}
+
+
+int main(){
+    
+    node *head= NULL;
+    
+    
+    head= insertend(head,10);
+    head= insertend(head,20);
+    head= insertend(head,30);
+    head= insertend(head,30);
+    head= insertend(head,999999);
+
+    cout<<"\nOriginal linked list"<<endl;
+    
+    print(head);
+    
+    deletenode(head);
+     
+    cout<<"\nlinked list after deleting node"<<endl;
+    print(head);
+    return 0;
+    
+}
