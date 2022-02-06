6fffda8f7efd8dbe57f6ba98f36ee2d22887c456
Sandeep Bhatt <sandeepb3287@gmail.com>
2022-02-06 19:49:40 +0530

    final code using floyd detection

---
+++ Basic Data structures/Linked List/detect-loop-linked-list.cpp
@@ -0,0 +1,93 @@
+// Using floyd cycle detection
+
+
+#include <iostream>
+using namespace std;
+struct node {
+// public:
+  int val;
+  node* next;
+    
+    node(int x)
+    {
+        val=x;
+        next=NULL;
+    }
+    
+};
+
+
+node *insertend( node *head,int x)
+{
+    
+    node *temp =new node(x);
+    
+    if(head==NULL)
+        return temp;
+    
+    node *cur=head;
+    while(cur->next!=NULL)
+        cur=cur->next;
+    
+    cur->next=temp;
+    
+     return head;
+        
+}
+
+
+
+
+// Recersive method
+
+bool isloop(node *head)
+{    
+    node *slow_p=head,*fast_p=head;
+    
+    while (fast_p!=NULL && fast_p->next!=NULL)
+    {
+        slow_p=slow_p->next;
+        fast_p=fast_p->next->next;
+        if(slow_p==fast_p)
+            return true;
+    }
+    return false;
+}
+
+
+void print( node *head)
+{
+  if(head==NULL)
+      return;
+    
+    cout<<head->val<<endl;
+
+    // recursive call 
+print(head->next);
+        
+}
+
+
+int main(){
+    
+    node *head= NULL;
+    
+    
+    head= insertend(head,10);
+    head= insertend(head,20);
+    head= insertend(head,30);
+    head= insertend(head,999999);
+
+    cout<<"\nOriginal linked list"<<endl;
+    
+    print(head);
+    // head= revlist(head,2);
+    
+    cout<<isloop(head);
+    
+    // cout<<"\nReverse of linked list"<<endl;
+    // print(head);
+    
+    return 0;
+    
+}
